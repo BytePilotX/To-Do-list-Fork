6be73f059851bf8676d00ccdfda4729a9ca65b36
Nitro-Live <100413904+BytePilotX@users.noreply.github.com>
2025-08-15 23:17:08 +0100

    Update To_Do_List_4.py

--- To_Do_List_4.py
+++ To_Do_List_4.py
@@ -1,43 +1,49 @@
+import sys
+import time
+
 def Task():
-    Task_list=[]
 
-    print("---WELECOM TO OUR TO DO LIST---")
+    Task_list = []
 
-    total_task=int(input("Enter How many task you want to enter :"))
+    print("---WELCOME TO OUR TO DO LIST!---")
+
+    total_task=int(input("Enter How many task you want to enter :\n"))
     for i in range(1,total_task+1):
-        task_name=input(f"Enter your {i} Task : ")
+        task_name=input(f"Enter your {i} Task : \n")
         Task_list.append(task_name)
 
-    print(f"Your, Today's Tasks are \n {Task_list}")
+    print(f"Today's Tasks are \n {Task_list}")
 
     while True:
-        Operation=int(input("Enter Operation :\n 1 - Add Task \n 2 - Update Task \n 3 - Delete Task \n 4 - View Task\n 5 - Exit Task"))
+        Operation = int(input("Enter Operation :\n 1 - Add Task \n 2 - Update Task \n 3 - Delete Task \n 4 - View Task\n 5 - Exit Task\n"))
 
-        if(Operation==1):
-          add = input("Enter what u want to addd : ")
+        if (Operation == 1):
+          add = input("Enter what u want to add : \n")
           Task_list.append(add)   
-          print(f"Your Task has been Successfully Added")
+          print("Your Task has been Successfully Added\n")
 
-        elif(Operation==2):
-          Update_Val = input("Enter the Task name what u want to Update : ")
+        elif (Operation == 2):
+          Update_Val = input("Enter the Task name what u want to Update : \n")
           if Update_Val in Task_list:
              up = input("Enter new Task : ")
              ind = Task_list.index(Update_Val)
              Task_list[ind] = up
-          print(f"Your Task has been Successfully Updated")
+          print("Your Task has been Successfully Updated\n")
 
-        elif(Operation==3):
-          delete = input("Enter what u want to Delete : ")
+        elif(Operation == 3):
+          delete = input("Enter what u want to Delete : \n")
 
           if delete in Task_list:
             Task_list.remove(delete)   
-            print(f"Your Task has been Successfully Deleted")
+            print("Your Task has been Successfully Deleted\n")
 
-        elif(Operation==4):
-          print(f"Your Tasks are {Task_list}")
+        elif(Operation == 4):
+          print(f"Your Tasks are {Task_list}\n")
+          time.sleep(3)
 
         elif(Operation==5):
-           print("Closing the program....")
-           break
+           print("Closing the program....\n")
+           sys.exit()
+
 
 Task()
